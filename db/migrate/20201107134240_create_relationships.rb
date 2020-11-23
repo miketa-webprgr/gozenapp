ce025d9122c1594a9107e327fbcd1a5200ff5989
miketa_webprgr <ksuedomi91@gmail.com>
2020-11-23 20:27:27 +0900

    とりあえず外部キーを外した

--- db/migrate/20201107134240_create_relationships.rb
+++ db/migrate/20201107134240_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.references :set_menu, null: false, foreign_key: true
-      t.references :food, null: false, foreign_key: true
+      t.references :set_menu, null: false, foreign_key: false
+      t.references :food, null: false, foreign_key: false
 
       t.timestamps
     end
